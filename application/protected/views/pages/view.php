fa9c3559a1f2fe95ef1a56f785a4e6c0f3a9fb26
Arsen <1b2c@mail.ru>
2014-06-07 02:55:12 +0400

    pages can view in front-end

---
+++ application/protected/views/pages/view.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this PagesController */
+/* @var $model Pages */
+
+$this->breadcrumbs=array(
+	'Pages'=>array('index'),
+	$model->title,
+);
+?>
+
+<?php echo $model->content; ?>
+
