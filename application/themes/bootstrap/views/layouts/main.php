6590f85e922377f1c6d81cbec02be5c0e4ea4d04
Arsen <1b2c@mail.ru>
2014-06-06 23:39:38 +0400

    added products to main menu

--- application/themes/bootstrap/views/layouts/main.php
+++ application/themes/bootstrap/views/layouts/main.php
@@ -35,6 +35,7 @@
 //                                        'class'=>'bootstrap.widgets.TbMenu',
                                         'items' => array(
                                             array('label' => 'Home', 'url' => array('/site/index')),
+                                            array('label'=>'Products', 'url'=>array('/products')),
                                             array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
                                             array('label' => 'Contact', 'url' => array('/site/contact')),
                                             array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
