77094cffe16179a01d7fdde3bc6a97a1eec0a249
vardush <vardush@mail.ru>
2014-06-11 00:43:29 +0400

    verj bug fix :D

--- application/protected/modules/admin/views/products/_form.php
+++ application/protected/modules/admin/views/products/_form.php
@@ -12,7 +12,7 @@
 	// controller action is handling ajax validation correctly.
 	// There is a call to performAjaxValidation() commented in generated controller code.
 	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>true,
+	'enableAjaxValidation'=>false,
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
@@ -46,7 +46,7 @@
 		<?php echo $form->hiddenField($model,'image'); ?>
 
     <div class="row">
-    <? $this->widget('ext.EAjaxUpload.EAjaxUpload',
+    <?php $this->widget('ext.EAjaxUpload.EAjaxUpload',
          array(
                'id'=>'EAjaxUpload',
                'config'=>array(
